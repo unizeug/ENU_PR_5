268ea8f9ff04b9fd0802758fd4aa1ee9da415365
Dirk <coolice.f.b@web.de>
2012-06-24 16:49:50 +0200

    Matlabdatei vergessen

--- ENU PR 5/Matlab/Quantisierungsfehler_8kHz_dreieck.m
+++ ENU PR 5/Matlab/Quantisierungsfehler_8kHz_dreieck.m
@@ -93,16 +93,16 @@ FFTshiftplotZP_autocorr(c, T_ges, f_T, 4, 'r', 5, 0, 200);
 
 % PDFs erstellen
 
-figure(1);
-print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Signal_Rekonstuiert.pdf
-figure(2);
-print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Signal_Rekonstuiert_delayed.pdf
-figure(3);
-print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Quantisierungsfehler.pdf
-figure(4);
-print -zbuffer -dpdf -r600 ../Bilder/8kHz_dreieck_Quant_Hist.pdf
-figure(5);
-print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_LSD.pdf
+% figure(1);
+% print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Signal_Rekonstuiert.pdf
+% figure(2);
+% print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Signal_Rekonstuiert_delayed.pdf
+% figure(3);
+% print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_Quantisierungsfehler.pdf
+% figure(4);
+% print -zbuffer -dpdf -r600 ../Bilder/8kHz_dreieck_Quant_Hist.pdf
+% figure(5);
+% print -painters -dpdf -r600 ../Bilder/8kHz_dreieck_LSD.pdf
 
 
 
